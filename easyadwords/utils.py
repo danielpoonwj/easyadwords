32c5c251c76593454b3e3a49a089dc93dc8e0a9c
Daniel Poon <daniel@wego.com>
2016-08-29 14:32:36 +0800

    added date_range

--- easyadwords/utils.py
+++ easyadwords/utils.py
@@ -1,4 +1,5 @@
 from suds.sudsobject import asdict
+from datetime import datetime, timedelta
 
 
 def serialize_soap_resp(resp):
@@ -26,3 +27,22 @@ def serialize_soap_resp(resp):
                 out[k] = v
 
     return out
+
+
+def date_range(start, end, ascending=True):
+    if isinstance(start, datetime):
+        start_date = start.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
+    else:
+        start_date = datetime.strptime(start, '%Y-%m-%d')
+
+    if isinstance(end, datetime):
+        end_date = end.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
+    else:
+        end_date = datetime.strptime(end, '%Y-%m-%d')
+
+    assert end_date >= start_date
+
+    days_apart = (end_date - start_date).days + 1
+
+    for i in (range(0, days_apart) if ascending else range(0, days_apart)[::-1]):
+        yield start_date + timedelta(i)
